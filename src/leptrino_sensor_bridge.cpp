d685733722a58555efe51a74b0d6670a0727c3d0
harumo11 <s_harumo@hotmail.co.jp>
2022-07-22 17:31:26 +0900

    this program become reconactable with user application.

--- src/leptrino_sensor_bridge.cpp
+++ src/leptrino_sensor_bridge.cpp
@@ -48,39 +48,62 @@ private:
 public:
     SensorValueSender(const unsigned int port = 50013)
     {
+        std::cout << "||| constructor" << std::endl;
         this->my_port = port;
+        this->prepare();
     };
 
     void prepare()
     {
+        this->socket.close();
+        std::cout << "||| Waiting the connection from user application." << std::endl;
         boost::asio::ip::tcp::acceptor acceptor(this->actx, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), this->my_port));
         acceptor.accept(this->socket);
+        std::cout << "||| Accepted by user application." << std::endl;
         ROS_INFO_STREAM("Remote info : " << this->socket.remote_endpoint());
         ROS_INFO_STREAM("Connection estublished with user application.");
     };
 
     void write(const std::string msg)
     {
+        std::cout << "||| write" << std::endl;
         boost::asio::write(this->socket, boost::asio::buffer(msg));
+        std::cout << "||| end write " << std::endl;
     };
 };
 
 int main(int argc, char *argv[])
 {
+    std::cout << "||| program starts" << std::endl;
     ros::init(argc, argv, "leptrino_sensor_bridge");
     ros::NodeHandle node_handle;
     ros::Rate control_hz(1000);
+    ROS_INFO_STREAM("Waiting for leptrino_force_torque/force_torque message");
+    ros::topic::waitForMessage<geometry_msgs::WrenchStamped>("/leptrino_force_torque/force_torque");
+    ROS_INFO_STREAM("Confirmed leptrino message");
 
     LeptrinoListener sensor;
     ros::Subscriber leptrino_subscriber = node_handle.subscribe("/leptrino_force_torque/force_torque", 1, &LeptrinoListener::callback, &sensor);
     SensorValueSender sender;
 
+    std::cout << "||| into the loop" << std::endl;
+
     while (ros::ok())
     {
         ROS_INFO_STREAM(sensor.flatten());
-        sender.write(sensor.flatten());
+        try
+        {
+            sender.write(sensor.flatten());
+        }
+        catch (const boost::system::system_error &e)
+        {
+            ROS_WARN_STREAM(e.what());
+            sender.prepare();
+        }
+
         ros::spinOnce();
         control_hz.sleep();
+        std::cout << "||| spin once" << std::endl;
     }
 
     return 0;
